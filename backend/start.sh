aa1896ea83df31dc12558f4eaa76714572468d92
Debojyoti Das <debojyoti072@gmail.com>
2026-06-19 19:55:08 +0530

    fix: switch to ytdl-patched to bypass Render datacenter IP blocks

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Fetching latest yt-dlp patch..."
-pip install --upgrade yt-dlp
+pip install -U https://github.com/ytdl-patched/ytdl-patched/archive/master.tar.gz
 
 echo "Starting Uvicorn..."
 # Port is read from the PORT environment variable injected by Render
